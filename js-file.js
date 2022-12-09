5145977ca1416d9eccd930516f812fbcdb412848
Tyler Pardee <tjpardee@gmail.com>
2022-12-09 14:04:58 -0500

    Create JS File to Populate Table Data

---
+++ js-file.js
@@ -0,0 +1,40 @@
+/* Populate Table */
+
+const fundDetails = [{
+    "detailType": "Fund Inception",
+    "value": "01/31/1990"
+},
+{
+    "detailType": "Category",
+    "value": "Aggressive Growth"
+},
+{
+    "detailType": "Management Style",
+    "value": "Passive"
+}, 
+{
+    "detailType": "Shares Outstanding",
+    "value": 860,977,443.60
+}, 
+{
+    "detailType": "Price to Earnings Ratio",
+    "value": 12.80
+}, 
+{
+    "detailType": "Price to Cash Flow",
+    "value": "8.14%"
+},
+{
+    "detailType": "Return to Equity",
+    "value": "23.07%"
+}, 
+{
+    "detailType": "Price to Book Ratio",
+    "value": 2.10
+}, 
+{
+    "detailType": "Weighted Average Market Capitalization",
+    "value": "$271.92B"
+}
+]
+
