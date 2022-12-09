0cbe441f8c2b867fa222c171ee34aa1ca163e62f
Tyler Pardee <tjpardee@gmail.com>
2022-12-09 15:16:18 -0500

    Update Functionality to Populate Table

--- js-file.js
+++ js-file.js
@@ -1,40 +1,58 @@
 /* Populate Table */
 
-const fundDetails = [{
-    "detailType": "Fund Inception",
-    "value": "01/31/1990"
-},
-{
-    "detailType": "Category",
-    "value": "Aggressive Growth"
-},
-{
-    "detailType": "Management Style",
-    "value": "Passive"
-}, 
-{
-    "detailType": "Shares Outstanding",
-    "value": 860,977,443.60
-}, 
-{
-    "detailType": "Price to Earnings Ratio",
-    "value": 12.80
-}, 
-{
-    "detailType": "Price to Cash Flow",
-    "value": "8.14%"
-},
-{
-    "detailType": "Return to Equity",
-    "value": "23.07%"
-}, 
-{
-    "detailType": "Price to Book Ratio",
-    "value": 2.10
-}, 
-{
-    "detailType": "Weighted Average Market Capitalization",
-    "value": "$271.92B"
-}
-]
+const fundDetails = [
+  {
+    detailType: "Fund Inception",
+    value: "01/31/1990",
+  },
+  {
+    detailType: "Category",
+    value: "Aggressive Growth",
+  },
+  {
+    detailType: "Management Style",
+    value: "Passive",
+  },
+  {
+    detailType: "Shares Outstanding",
+    value: "860,977,443.60",
+  },
+  {
+    detailType: "Price to Earnings Ratio",
+    value: 12.8,
+  },
+  {
+    detailType: "Price to Cash Flow",
+    value: "8.14%",
+  },
+  {
+    detailType: "Return to Equity",
+    value: "23.07%",
+  },
+  {
+    detailType: "Price to Book Ratio",
+    value: 2.1,
+  },
+  {
+    detailType: "Weighted Average Market Capitalization",
+    value: "$271.92B",
+  },
+];
 
+var details = document.querySelector(".fund-details");
+var table = document.createElement("table");
+
+details.appendChild(table);
+
+for (i = 0; i < 9; i++) {
+  var tableRow = document.createElement("tr");
+  var cellDetailType = document.createElement("td");
+  var cellValue = document.createElement("td");
+
+  cellDetailType.innerHTML = `${fundDetails[i].detailType}`;
+  cellValue.innerHTML = `${fundDetails[i].value}`;
+
+  table.appendChild(tableRow);
+  tableRow.appendChild(cellDetailType);
+  tableRow.appendChild(cellValue);
+}
